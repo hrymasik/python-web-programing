bb531b9741cf1ef80ce16b830db2ec47abc9f6ef
Masik :) <grinakmaksim@gmail.com>
2019-11-21 21:37:16 +0200

    Created some other shit!

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello, world!"
+
+@app.route('/maksym')
+def maksym():
+    return "Hello, Maksym!"
+
+@app.route("/<string:name>")
+def hello(name):
+    name = name.capitalize()
+    return f"Hello, {name}!"
